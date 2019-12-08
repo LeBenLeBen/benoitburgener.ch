29d0af50ea43509211a8b0007e73c65c7f85bed0
Benoît Burgener <benoit.burgener@gmail.com>
2019-12-08 15:18:33 +0100

    Optimize CSS only in production
    Otherwise using new classes requires to rebuild CSS twice

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,24 @@
-module.exports = {
+const config = {
   plugins: [
     require('postcss-import'),
     require('tailwindcss'),
     require('precss'),
     require('autoprefixer'),
+  ],
+};
+
+if (process.env.NODE_ENV === 'production') {
+  config.plugins.push(
     require('@fullhuman/postcss-purgecss')({
       content: ['dist/**/*.html'],
       defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
-    }),
+    })
+  );
+  config.plugins.push(
     require('cssnano')({
       preset: 'default',
-    }),
-  ],
-};
+    })
+  );
+}
+
+module.exports = config;
